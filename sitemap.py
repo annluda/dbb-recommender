cfac46c4f61050406a7d482723bb57defc675ee5
anld <anld@sugon.com>
2019-07-30 11:45:58 +0800

    多线程解析sitemaps

--- sitemap.py
+++ sitemap.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 import requests
 import gzip
+import zlib  # gzip python2和3代码不通用 所以该用zlib
 import time
 from lxml import etree
 from threading import Thread
-from queue import Queue
-from models.tags import Tags
-from models.books import Books
+from Queue import Queue  # python2
+# from queue import Queue  # python3
+from mysql_operation import bulk_insert_books
 
 
 class SitemapSpider(Thread):
-    def __init__(self, name, url_queue):
+    def __init__(self, url_queue):
         super(SitemapSpider, self).__init__()
-        self.name = name
         self.url_queue = url_queue
 
     def run(self):
-        print(self.name, 'started')
+        print(self.name, 'start')
         while not self.url_queue.empty():
             self.books()
 
@@ -24,16 +24,14 @@ class SitemapSpider(Thread):
         s_time = time.time()
         sitemap = self.url_queue.get()
         response = requests.get(sitemap)
-        data = gzip.decompress(response.content)
+        # data = gzip.decompress(response.content)
+        data = zlib.decompress(response.content, 16 + zlib.MAX_WBITS)
         selector = etree.HTML(data)
         urls = selector.xpath('//loc/text()')
-        for url in urls:
-            if url.startswith('https://book.douban.com/tag/'):
-                tag = Tags(tag=url.split('/')[4])
-                tag.upload()
-            elif url.startswith('https://book.douban.com/subject/'):
-                book = Books(id=url.split('/')[4])
-                book.upload()
+
+        data = [a.split('/')[4] for a in urls if a.startswith('https://book.douban.com/subject/')]
+        bulk_insert_books(data)
+
         e_time = time.time()
         print(sitemap, '耗时 %dmin%02ds' % divmod(e_time - s_time, 60))
         print(self.url_queue.qsize(), 'left')
@@ -57,12 +55,11 @@ if __name__ == '__main__':
     t1 = time.time()
     spider_list = []
     for i in range(10):
-        spider = SitemapSpider('spider' + str(i), sitemap_queue)
+        spider = SitemapSpider(sitemap_queue)
         spider.start()
         spider_list.append(spider)
     for spider in spider_list:
         spider.join()
 
-    print('all finished')
     t2 = time.time()
-    print('总耗时 %dmin%02ds' % divmod(t2 - t1, 60))
+    print(u'总耗时 %dmin%02ds' % divmod(t2 - t1, 60))
