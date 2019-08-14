c082d0f4ee053be12d1f0949c93d5907625cc49c
anld <anld@sugon.com>
2019-08-14 16:14:05 +0800

    iterm pipelines

--- daq/scrapy/douban/middlewares.py
+++ daq/scrapy/douban/middlewares.py
@@ -1,8 +1,9 @@
 #! -*- encoding:utf-8 -*-
-from twisted.internet.error import ConnectionRefusedError
-from twisted.internet.error import TimeoutError
-from twisted.internet.error import TCPTimedOutError
+# from twisted.internet.error import ConnectionRefusedError
+# from twisted.internet.error import TimeoutError
+# from twisted.internet.error import TCPTimedOutError
 from .spiders import db_query
+import random
 
 
 class ProxyMiddleware(object):
@@ -10,15 +11,15 @@ class ProxyMiddleware(object):
         self.ip = ''
 
     def process_request(self, request, spider):
-        self.ip = db_query.query('select ip from proxy_ip')[0][0]
+        self.ip = db_query.query('select ip from proxy_ip')[random.choice([0, 1])][0]
         request.meta['proxy'] = 'http://' + self.ip.strip()
         return
 
-    def process_exception(self, request, exception, spider):
-        if isinstance(exception, (ConnectionRefusedError, TimeoutError, TCPTimedOutError)):
-            pass
-        else:
-            spider.logger.warn(type(exception))
-        self.ip = db_query.query('select ip from proxy_ip')[1][0]
-        request.meta['proxy'] = 'http://' + self.ip.strip()
-        return request
+    # def process_exception(self, request, exception, spider):
+    #     if isinstance(exception, (ConnectionRefusedError, TimeoutError, TCPTimedOutError)):
+    #         pass
+    #     else:
+    #         spider.logger.warning(type(exception))
+    #     self.ip = db_query.query('select ip from proxy_ip')[1][0]
+    #     request.meta['proxy'] = 'http://' + self.ip.strip()
+    #     return request
